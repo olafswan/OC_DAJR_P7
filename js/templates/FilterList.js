b4f0f957346252c5ac672e3361d9c73d9c463bf0
olafswan <juliendupdupdup@gmail.com>
2023-08-13 18:52:42 +0200

    création classe pour mise à jour  des filtres

---
+++ js/templates/FilterList.js
@@ -0,0 +1,25 @@
+class FilterList {
+  constructor(ingredientsList, appliancesList, ustensilsList) {
+    this._ingredientsList = ingredientsList.sort();
+    this._appliancesList = appliancesList.sort();
+    this._ustensilsList = ustensilsList.sort();
+  }
+
+  filterFiller(listId, listName) {
+    const filterContainer = document.querySelector(`#${listName}`);
+    const $wrapper = filterContainer.querySelector(".select-options-container");
+
+    listId.forEach((item) => {
+      const option = document.createElement("p");
+      option.classList.add("select-option");
+      option.textContent = item;
+      $wrapper.appendChild(option);
+    });
+  }
+
+  fillFiltersLists() {
+    this.filterFiller(this._ingredientsList, "ingredients");
+    this.filterFiller(this._appliancesList, "appliances");
+    this.filterFiller(this._ustensilsList, "ustensils");
+  }
+}
